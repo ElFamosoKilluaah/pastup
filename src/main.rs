ff4c504588adba5d83a63e05d4293bc74a66c942
Quentincestino <38379985+ElFamosoKilluaah@users.noreply.github.com>
2020-07-13 21:10:23 +0200

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-extern crate structopt;
-extern crate pastebin_rs;
-
 use std::path::PathBuf;
-use std::fs::File;
 
 use structopt::StructOpt;
-use pastebin_rs::PastebinBuilder;
+use pastebin_rs::{ PastebinBuilder, VisibilityLevel };
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "pastup")]
@@ -19,29 +15,46 @@ struct PastupCli {
     #[structopt(short = "f", long = "format")]
     format: Option<String>,
 
+    /// Sets the visibility of the paste
+    #[structopt(short = "v", long = "visibility")]
+    visibility: Option<String>,
+
     /// Sets the lifetime of the paste
     #[structopt(short = "l", long = "lifetime")]
     lifetime: Option<String>,
 
-    /// Sets the paste's title
+    /// Sets the paste's name
     #[structopt(short = "t", long = "title")]
-    title: Option<String>,
+    name: Option<String>,
 }
 
 fn main() {
     let opt: PastupCli = PastupCli::from_args();
     let api_key = format!(env!("PASTEBIN_API_KEY");
     
+    let visibility = match opt.visibility {
+        Some(v) => parse_visibility_string(v),
+        None => None,
+    };
+
+
     if let Ok(text) = std::fs::read_to_string(&opt.path) {
-        
+        // let builder = PastebinBuilder::new(
+        //     api_key,
+        //     text,
+        //     opt.name,
+
+
+        // )
     }
     
     else {
         eprintln!("Error: the file {:?} doesn\'t contain valid UTF-8.", &opt.path);
     }
-    
-    
-    // let builder = PastebinBuilder::new(api_key, , paste_name: Option<String>, visibility: Option<VisibilityLevel>, format: Option<String>, expire_date: Option<ExpirationDate>)
+}
 
+fn parse_visibility_string(to_parse: String) -> Option<VisibilityLevel> {
+    match to_parse {
 
+    }
 }
